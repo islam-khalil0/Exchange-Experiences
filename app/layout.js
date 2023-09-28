bee7830194300bd30e0e3474e6f6044add1f5a2c
islam <eslamkhalil851@gmail.com>
2023-09-28 21:55:28 +0300

    fixe two

--- app/layout.js
+++ app/layout.js
@@ -2,17 +2,22 @@ import Nav from "./components/Nav";
 import { AuthContextProvider } from "./context/AuthContext";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Exchange experiences",
-  description: "application for exchanging experiences",
-};
+// export const metadata = {
+//   title: "Exchange experiences",
+//   description: "application for exchanging experiences",
+// };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Exchange Experiences</title>
+        <meta name="keyWords" content="Exchange Experiences App"></meta>
+      </Head>
       <body className={inter.className}>
         <AuthContextProvider>
           <Nav />
